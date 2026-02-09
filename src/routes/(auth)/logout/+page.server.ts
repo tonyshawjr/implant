72a7d0651ff4c69cf7e1ea4f87539514581fcb2f
tonyshawjr <tonyjr.design@gmail.com>
2026-02-09 10:45:19 -0500

    Fix logout to work with GET request

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -2,7 +2,16 @@ import { lucia } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ locals, cookies }) => {
+  // Allow GET request to logout
+  if (locals.session) {
+    await lucia.invalidateSession(locals.session.id);
+    const sessionCookie = lucia.createBlankSessionCookie();
+    cookies.set(sessionCookie.name, sessionCookie.value, {
+      path: '.',
+      ...sessionCookie.attributes
+    });
+  }
   throw redirect(302, '/login');
 };
 
